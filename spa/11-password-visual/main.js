7db4ace77451b87ca3955a1273c78175874ad859
Liyue1998 <1757759700@qq.com>
2019-04-03 16:36:14 +0800

    语法高亮

--- spa/11-password-visual/main.js
+++ spa/11-password-visual/main.js
@@ -1,7 +1,16 @@
 $(function(){
-  var $eye=$("#eye");
+    var $img=$('img');
+    var $inp=$('input');
+    $img.mouseover(()=>{
+      $inp.attr("type","text");
+      $img.attr("src","open.png");
+                  
+    });
+    $img.mouseout(()=>{
+      $inp.attr('type',"password");
+      $img.attr('src',"close.png");
+                  
+    })
+    console.log('hello world');
 
-  $eye.mouseover(function(){
-    
-  });
 });
